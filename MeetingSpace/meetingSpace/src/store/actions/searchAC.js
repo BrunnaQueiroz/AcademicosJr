5d63c77e2b7993a9ea642e345cbcc1803b9dd16e
“[firstname <[valid-email]>
2023-01-16 17:05:09 -0300

    Create searchAC.js

---
+++ MeetingSpace/meetingSpace/src/store/actions/searchAC.js
@@ -0,0 +1,53 @@
+import youtubeSearch from 'youtube-api-v3-search'
+
+import YTApi from '../../services/api'
+
+import API_KEY from '../../services/api'
+
+const API_KEY = YTApi
+
+export const searchVideo = (params) => {
+    return dispatch => {
+        dispatch(searchVideoStart())
+        youtubeSearch(API_KEY, {q: params})
+
+        .then((data) => dispatch (searchVideoSuccess(data.items)))
+        .catch(() => dispatch (searchVideoError()))
+    }
+}
+
+
+export const searchVideoStart = () => {
+    return {
+        type: "SEARCH_VIDEO",
+        loading: true,
+        error: false
+    }
+}
+
+export const searchVideoSuccess = (videos) => {
+    return {
+        type: "SEARCH_VIDEO_SUCCESS",
+        videos,
+        loading: false,
+        error: false
+    }
+}
+
+export const searchVideoError = () => {
+    return{
+        type: "SEARCH_VIDEO_ERROR",
+        loading: false,
+        error: true
+    }
+}
+
+export const buscaVideo = (params) => {
+    return dispatch => {
+        dispatch(searchVideoStart())
+        youtubeSearch(API_KEY, { q: params })
+            .then((data) => dispatch(searchVideoSuccess(data.items)))
+            .catch(() => dispatch(searchVideoError()))
+            
+    }
+}
