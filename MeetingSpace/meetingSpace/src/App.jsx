930d45619565719d07d8427b7171209128405f13
“[firstname <[valid-email]>
2023-01-16 17:04:55 -0300

    Update App.jsx

--- MeetingSpace/meetingSpace/src/App.jsx
+++ MeetingSpace/meetingSpace/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import MeetingRoom from './pages/MeetingRoom/MeetingRoom'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
 
   return (
     <div className="App">
-     
+      <MeetingRoom />
+      oi
     </div>
   )
 }
