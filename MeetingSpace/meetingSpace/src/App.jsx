fe37470f18dbac2d8ea144c4495636184fc23f53
“[firstname <[valid-email]>
2023-01-17 12:46:55 -0300

    Update App.jsx

--- MeetingSpace/meetingSpace/src/App.jsx
+++ MeetingSpace/meetingSpace/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import MeetingRoom from './pages/MeetingRoom/MeetingRoom'
+import Login from './pages/LoginPage/Login'
 
 function App() {  
 
   return (
     <div className="App">
-      <MeetingRoom />      
+      <MeetingRoom />     
+      <Login /> 
     </div>
   )
 }
