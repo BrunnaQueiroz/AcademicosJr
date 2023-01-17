902ad53913c8cc71163cbf3a48e385a1cfe55873
“[firstname <[valid-email]>
2023-01-17 12:47:07 -0300

    Update server.js

--- MeetingSpace/meetingSpace/src/server/server.js
+++ MeetingSpace/meetingSpace/src/server/server.js
@@ -1,9 +1,9 @@
 /*facilitates the applications's create*/
-const koa  =require('koa')
+const Koa  = require('koa')
 const http = require('http')
 const socket = require('socket.io')
 
-const app = new koa()
+const app = new Koa()
 /*callback() is a koa's function that send us a function
 that know work with requires*/
 const server = http.createServer(app.callback())
@@ -11,6 +11,8 @@ const io = socket(server)
 
 const SERVER_HOST = 'localhost'
 const SERVER_PORT = 8080
+
+
 
 io.on('connection', socket => {
     console.log('[IO] Connection => new conection')
