734f3c54467ed4ae1b59a8cacdc724ca730a949c
“[firstname <[valid-email]>
2023-01-18 01:30:42 -0300

    Update App.jsx

--- MeetingSpace/meetingSpace/src/App.jsx
+++ MeetingSpace/meetingSpace/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { ReactDOM } from 'react-dom/client'
 import './App.css'
 import MeetingRoom from './pages/MeetingRoom/MeetingRoom'
 import Login from './pages/LoginPage/Login'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {  
 
   return (
     <div className="App">
-      <Login /> 
-      <MeetingRoom />     
+      {/*<Login /> 
+      <MeetingRoom />   */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/Room" element={<MeetingRoom/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
