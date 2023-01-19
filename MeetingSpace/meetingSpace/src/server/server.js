92011d4b5d7ad1333f0e4f4685bcbd596bdfc350
“[firstname <[valid-email]>
2023-01-19 12:10:57 -0300

    Update server.js

--- MeetingSpace/meetingSpace/src/server/server.js
+++ MeetingSpace/meetingSpace/src/server/server.js
@@ -1,21 +1,53 @@
 /*facilitates the applications's create*/
-const Koa  = require('koa')
-const http = require('http')
-const socket = require('socket.io')
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const { ExpressPeerServer } = require('peer') /**/
+const peerServer = ExpressPeerServer(server, {
+    debug:false
+}); /**/
+const { response } = require('express');/**/
+const io = require('socket.io')(server)
+const { v4 : uuidV4 } = require('uuid')
 
-const app = new Koa()
-/*callback() is a koa's function that send us a function
-that know work with requires*/
-const server = http.createServer(app.callback())
-const io = socket(server)
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-const SERVER_HOST = 'localhost'
-const SERVER_PORT = 8080
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
-
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
 io.on('connection', socket => {
-    console.log('[IO] Connection => new conection')
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    /*socket.broadcast.to(roomId).emit('user-connected',userId)*/
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+        /*socket.broadcast.to(roomId).emit('user-disconnected',userId)*/
+        socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+
+    socket.on('changevideo', (videoId) => {
+      io.emit('changevideo', videoId)
+    })
+
+    socket.on('play', () => {
+      io.sockets.in(roomId).emit("play")
+    })
+
+    socket.on('pause', () => {
+      io.sockets.in(roomId).emit("pause")
+    })
+  })
+})
+
+
+io.on('connection', socket => {    
     socket.on('chat.message', data => {
         console.log('[SOCKET] chat.message => ', data)
     })
@@ -24,7 +56,4 @@ io.on('connection', socket => {
     })
 })
 
-/*PORT, HOST AND CALLBACK FUNCTION*/
-server.listen(SERVER_PORT, SERVER_HOST, () => {
-    console.log(`[HTTP] Listen => Server is running at http://${SERVER_HOST}:${SERVER_PORT}`)     
-})
+server.listen(3000)
